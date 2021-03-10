01cc5bdb53c4621da516b75648eaff47167f73d8
José Catalá <jfca68@gmail.com>
2021-03-10 09:08:01 +0000

    Initial Commit

---
+++ iMine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMine
+//
+//  Created by Jose Catala on 10/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
